b227b76f84960be468c257a6ac3c1f5b394ee817
nhattruc512-hub <nhattruc512@gmail.com>
2026-08-20 14:00:45 +0700

    refactor: remove demo data from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { ClassFinder } from "@/components/class-finder";
-import { demoClasses } from "@/lib/mock-classes";
 
 export default function Home() {
-  return <ClassFinder initialClasses={demoClasses} />;
+  return <ClassFinder initialClasses={[]} />;
 }
